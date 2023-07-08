85c051ac172bb80ca2412576e75767bd3974bc7f
hafidzpsb <randombs51102@gmail.com>
2023-07-08 15:44:50 +0700

    footer

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,29 +1,38 @@
 <!doctype html>
 <html>
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-        <link rel="stylesheet" href="{{ URL::asset('assets/css/app.css') }}">
-        <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet' type='text/css'>
-    </head>
     <body>
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-3 my-3 mx-4">
-                    <h4>Kontak Kami</h4>
+        <div class="container-fluid" style="box-shadow: 4px 6px 12px rgba(0,0,0,0.4);">
+            <div class="row" style="background: #fae8d2;">
+                <div class="col-4 my-3 mx-4">
+                    <h4 class="border-bottom border-black"><b>Kontak Kami</b></h4>
+                    <p style="text-align: justify;">Komplek Perkantoran Pemda, Jl. Somawinata, Kel. Kadu Agung, Kec. Tigaraksa, Kadu Agung, Kec. Tigaraksa, Kabupaten Tangerang, Banten 15720<br><br>
+                    <b>Telepon: (021) 5990130</b></p>
                 </div>
-                <div class="col-3 my-3 mx-4">
-                    <h4>Tautan</h4>
+                <div class="col-4 my-3 mx-4">
+                    <h4 class="border-bottom border-black"><b>Tautan</b></h4>
+                    <ul class="list-unstyled mb-0">
+                        <li class="mb-2"><a class="hover-underline-animation text-reset text-decoration-none" href="https://www.kejaksaan.go.id/">Kejaksaan Republik Indonesia</a></li>
+                        <li class="mb-2"><a class="hover-underline-animation text-reset text-decoration-none" href="https://www.kejati-banten.go.id/">Kejaksaan Tinggi Banten</a></li>
+                        <li class="mb-2"><a class="hover-underline-animation text-reset text-decoration-none" href="https://tangerangkab.go.id/">PEMDA Kabupaten Tangerang</a></li>
+                        <li class="mb-2"><a class="hover-underline-animation text-reset text-decoration-none" href="https://tangerangkab.go.id/setwan">DPRD Kabupaten Tangerang</a></li>
+                        <li class="mb-2"><a class="hover-underline-animation text-reset text-decoration-none" href="">Polres Metro Kabupaten Tangerang </a></li>
+                        <li class="mb-2"><a class="hover-underline-animation text-reset text-decoration-none" href="https://pn-tangerang.go.id/">Pengadilan Negeri Tangerang</a></li>
+                        <li class="mb-2"><a class="hover-underline-animation text-reset text-decoration-none" href="https://kodamjaya-tniad.mil.id/kodim0510/">KODIM 0510 Tigaraksa</a></li>
+                    </ul>
                 </div>
-                <div class="col-3 my-3 mx-4 text-center">
+                <div class="col-2 my-3 mx-4 text-center">
                     <img class="d-block mx-auto mb-3" src="assets/images/logo.png" alt="" width="150" height="150">
                     <h6>Kejaksaan Negeri Kabupaten Tangerang</h4>
                 </div>
             </div>
-            <div class="row">
+            <div class="row" style="background: #fae8d2">
+                <div class="col-4 my-3 mx-4">
+                    <h4 class="border-bottom border-black"><b>Statistik Pengunjung</b></h4>
+                    <a href="https://info.flagcounter.com/nhnv">
+                        <img src="https://s11.flagcounter.com/count/nhnv/bg_FAE8D2/txt_454545/border_FAE8D2/columns_2/maxflags_10/viewers_0/labels_1/pageviews_1/flags_0/percent_0/" alt="Flag Counter" border="0">
+                    </a>
+                </div>
             </div>
         </div>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
     </body>
 </html>
