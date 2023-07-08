9b1dcbb5290e9793539e06fb88477a41588bedac
hafidzpsb <randombs51102@gmail.com>
2023-07-08 16:15:03 +0700

    fix

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -3,13 +3,13 @@
     <body>
         <div class="container-fluid" style="box-shadow: 4px 6px 12px rgba(0,0,0,0.4);">
             <div class="row" style="background: #fae8d2;">
-                <div class="col-4 my-3 mx-4">
-                    <h4 class="border-bottom border-black"><b>Kontak Kami</b></h4>
+                <div class="col-5 my-4 mx-4">
+                    <h4 class="border-bottom border-black py-2"><b>Kontak Kami</b></h4>
                     <p style="text-align: justify;">Komplek Perkantoran Pemda, Jl. Somawinata, Kel. Kadu Agung, Kec. Tigaraksa, Kadu Agung, Kec. Tigaraksa, Kabupaten Tangerang, Banten 15720<br><br>
                     <b>Telepon: (021) 5990130</b></p>
                 </div>
-                <div class="col-4 my-3 mx-4">
-                    <h4 class="border-bottom border-black"><b>Tautan</b></h4>
+                <div class="col-3 my-4 mx-4">
+                    <h4 class="border-bottom border-black py-2"><b>Tautan</b></h4>
                     <ul class="list-unstyled mb-0">
                         <li class="mb-2"><a class="hover-underline-animation text-reset text-decoration-none" href="https://www.kejaksaan.go.id/">Kejaksaan Republik Indonesia</a></li>
                         <li class="mb-2"><a class="hover-underline-animation text-reset text-decoration-none" href="https://www.kejati-banten.go.id/">Kejaksaan Tinggi Banten</a></li>
@@ -20,17 +20,29 @@
                         <li class="mb-2"><a class="hover-underline-animation text-reset text-decoration-none" href="https://kodamjaya-tniad.mil.id/kodim0510/">KODIM 0510 Tigaraksa</a></li>
                     </ul>
                 </div>
-                <div class="col-2 my-3 mx-4 text-center">
-                    <img class="d-block mx-auto mb-3" src="assets/images/logo.png" alt="" width="150" height="150">
+                <div class="col-2 my-4 mx-4 text-center">
+                    <img class="mb-3" src="assets/images/logo.png" alt="" width="150" height="150">
                     <h6>Kejaksaan Negeri Kabupaten Tangerang</h4>
                 </div>
             </div>
             <div class="row" style="background: #fae8d2">
-                <div class="col-4 my-3 mx-4">
-                    <h4 class="border-bottom border-black"><b>Statistik Pengunjung</b></h4>
+                <div class="col-5 my-4 mx-4">
+                    <h4 class="border-bottom border-black py-2"><b>Statistik Pengunjung</b></h4>
                     <a href="https://info.flagcounter.com/nhnv">
                         <img src="https://s11.flagcounter.com/count/nhnv/bg_FAE8D2/txt_454545/border_FAE8D2/columns_2/maxflags_10/viewers_0/labels_1/pageviews_1/flags_0/percent_0/" alt="Flag Counter" border="0">
                     </a>
+                </div>
+            </div>
+            <div class="row" style="background: #fae8d2">
+                <div class="container border-top border-black">
+                    <div class="row" style="background: #fae8d2">
+                        <div class="col-6 my-2">
+                            <small>Copyright © Kejaksaan Negeri Kabupaten Tangerang</small>
+                        </div>
+                        <div class="col-6 my-2"  style="text-align: right;">
+                            <small>Managed by IT Team</small>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
